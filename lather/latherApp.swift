570be54a167c4fde3799288fc4e5cecdfeaf67fb
emir <emirhassan.ali02@gmail.com>
2026-09-15 09:39:56 -0300

    Initial Commit

---
+++ lather/latherApp.swift
@@ -0,0 +1,17 @@
+//
+//  latherApp.swift
+//  lather
+//
+//  Created by emir on 15/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct latherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
